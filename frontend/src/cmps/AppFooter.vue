7bb9bc4dc24e8cd318497b50df1e75a929d72491
StrukovSergei <strukov.sergei.999@gmail.com>
2023-07-25 20:39:17 +0300

    second commit

---
+++ frontend/src/cmps/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+        <div class="footer-logo">fiverr</div>
+        <nav>
+            <RouterLink to="/">
+                <span role="img" aria-label="logo">🙏</span>
+            </RouterLink>
+            <RouterLink to="/explore">Explore</RouterLink>
+            <RouterLink to="/review">Reviews</RouterLink>
+            <RouterLink to="/chat">Chat</RouterLink>
+            <RouterLink to="/login">Login / Signup</RouterLink>
+        </nav>
+    </footer>
+</template>
+<script>
+export default {
+    computed: {
+
+    }
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,28 +1,75 @@
 <template>
-  <section class="container home text-center">
-    <h1>Home Page</h1>
-    <h5 class="text-center">
-      Powered up Frontend 
-    </h5>
-    <img alt="Vue logo" src="../assets/logo.png">
+  <section class="container home ">
+    <h1 class="text-center">Home Page</h1>
+    <h5 class="text-center">Trusted by:</h5>
+    <section>
+      <h2>Popular services:</h2>
+      
+    </section>
+    <vueper-slides bullets:false class="no-shadow slides-container" :visible-slides="5" slide-multiple :gap="2"
+      :slide-ratio="1 / 4" :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
+      <vueper-slide bullets:false class="slides" v-for="i in 10" :key="i" :title="i.toString()" />
+    </vueper-slides>
   </section>
-
-  
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
 export default {
   name: 'home',
-  data() {
-    return {
-    }
-  },
+  data: () => ({
+    slides: [
+      {
+        title: 'Slide #1',
+        content: 'Slide 1 content.'
+      },
+      {
+        title: 'Slide #2',
+        content: 'Slide 2 content.'
+      },
+      {
+        title: 'Slide #3',
+        content: 'Slide 3 content.'
+      },
+      {
+        title: 'Slide #4',
+        content: 'Slide 4 content.'
+      },
+      {
+        title: 'Slide #5',
+        content: 'Slide 5 content.'
+      },
+      {
+        title: 'Slide #6',
+        content: 'Slide 6 content.'
+      },
+      {
+        title: 'Slide #7',
+        content: 'Slide 7 content.'
+      },
+      {
+        title: 'Slide #8',
+        content: 'Slide 8 content.'
+      },
+      {
+        title: 'Slide #9',
+        content: 'Slide 9 content.'
+      },
+      {
+        title: 'Slide #10',
+        content: 'Slide 10 content.'
+      }
+    ]
+  }),
   computed: {
   },
   created() {
   },
   methods: {
-  }
-  
+  },
+  components: { VueperSlides, VueperSlide },
+
 }
 </script>
