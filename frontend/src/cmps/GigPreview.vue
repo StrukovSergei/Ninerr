c900a56a2194ff525df7e35877c2b89a7fd9414e
DanielNhm <Danielnahumm@gmail.com>
2023-07-28 12:08:39 +0300

    try to implemnt the carousel

--- frontend/src/cmps/GigPreview.vue
+++ frontend/src/cmps/GigPreview.vue
@@ -1,65 +1,71 @@
 <template>
     <section>
-        <!-- <ul class="gig-list"> -->
-            <li class="gig-preview" @click="goToDetail">
-                <img :src="gig.imgUrls[0]" class="product_img"/>
-            <section class="user_preview">
-                <div class="user_info">
-                    <img class="user_img" :src="gig.owner.imgUrl" />
-                    <a class="user_name txt_body">{{ gig?.owner.fullname }}</a>
-                </div>
-            <div>
-                <h3 class="level txt_body">{{ gig.owner.level }}</h3>
-            </div>
+      <li class="gig-preview" @click="goToDetail">
+        <vueper-slides
+          ref="vueperslides1"
+          :touchable="false"
+          fade
+          :autoplay="false"
+          :bullets="false"
+          fixed-height="400px"
+        >
+          <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide"></vueper-slide>
+        </vueper-slides>
+  
+        <section class="user_preview">
+          <div class="user_info">
+            <img class="user_img" :src="gig.owner.imgUrl" />
+            <a class="user_name txt_body">{{ gig?.owner.fullname }}</a>
+          </div>
+          <div>
+            <h3 class="level txt_body">{{ gig.owner.level }}</h3>
+          </div>
         </section>
-            <h3 class="title_product txt_body">{{ gig?.title }}</h3>
-            <div class ="rate">
-             <span v-html="$svg('blackStar')"></span> 
-            <p class="txt_body">{{ gig?.owner.rate }}</p>
-            </div>            
-          <p class="price_product txt_body">
-               From ${{ gig.price?.toLocaleString() }}
-            </p>
-            <!-- <button @click="removeGig(gig._id)">x</button> -->
-            <hr />
-        </li>
-        <!-- </ul> -->
-        <!-- <form @submit.prevent="addGig()">
-            <h2>Add gig</h2>
-            <input type="text" v-model="gigToAdd.title" />
-            <button>Save</button>
-        </form> -->
-
+  
+        <h3 class="title_product txt_body">{{ gig?.title }}</h3>
+  
+        <div class="rate">
+          <span v-html="$svg('blackStar')"></span>
+          <p class="txt_body">{{ gig?.owner.rate }}</p>
+        </div>
+  
+        <p class="price_product txt_body">
+          From ${{ gig.price?.toLocaleString() }}
+        </p>
+        <!-- <button @click="removeGig(gig._id)">x</button> -->
+        <hr />
+      </li>
     </section>
-</template>
-
-<script>
-import { gigService } from '../services/gig.service.local'
-import { getActionRemoveGig, getActionUpdateGig } from '../store/gig.store'
-export default {
+  </template>
+  
+  <script>
+  import { VueperSlides, VueperSlide } from 'vueperslides';
+  import 'vueperslides/dist/vueperslides.css';
+  
+  export default {
     name: 'GigPreview',
     props: {
-        gig: Object,
+      gig: Object,
     },
     data() {
-        return {
-            gigToAdd: gigService.getEmptyGig()
-        }
+      return {
+        slides: [],
+      };
     },
     computed: {
-        gigs() {
-            return this.$store.getters.gigs
-        }
+      gigs() {
+        return this.$store.getters.gigs;
+      },
     },
     created() {
-        this.$store.dispatch({ type: 'loadGigs' })
+      this.slides = this.gig.imgUrls;
     },
     methods: {
-        goToDetail() {
-            this.$router.push(`/gig/${this.gig._id}`)
-        },
-
+      goToDetail() {
+        this.$router.push(`/gig/${this.gig._id}`);
+      },
     },
-}
-
-</script>
+    components: { VueperSlides, VueperSlide },
+  };
+  </script>
+  
