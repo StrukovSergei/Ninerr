95f5c310fedaafdaf70448ff62f8508bf69341b8
Shlomi Levi <shlomilevi82@gmail.com>
2023-07-28 16:24:12 +0300

    new hero pics

--- frontend/src/cmps/GigPreview.vue
+++ frontend/src/cmps/GigPreview.vue
@@ -1,30 +1,42 @@
 <template>
     <!-- <section> -->
-        <!-- <ul class="gig-list"> -->
-            <li class="gig-preview" @click="goToDetail">
-                <img :src="gig.imgUrls[0]" class="product_img"/>
-            <section class="user_preview">
-                <div class="user_info">
-                    <img class="user_img" :src="gig.owner.imgUrl" />
-                    <a class="user_name txt_body">{{ gig?.owner.fullname }}</a>
-                </div>
+    <!-- <ul class="gig-list"> -->
+    <li class="gig-preview" @click="goToDetail">
+        <vueper-slides @click.native="preventClick" ref="vueperslides1" arrows-outside :touchable="false" fade
+            :autoplay="false" @slide="$refs.vueperslides2.goToSlide($event.currentSlide.index, { emit: false })"
+            fixed-height="400px">
+            <template #arrow-left>
+                <i class="fa-solid fa-angle-left"></i>
+            </template>
+            <template #arrow-right>
+                <i class="fa-solid fa-angle-right"></i>
+            </template>
+            <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.image">
+                <!-- <img :src="gig.imgUrls[0]" class="product_img" /> -->
+            </vueper-slide>
+        </vueper-slides>
+        <section class="user_preview">
+            <div class="user_info">
+                <img class="user_img" :src="gig.owner.imgUrl" />
+                <a class="user_name txt_body">{{ gig?.owner.fullname }}</a>
+            </div>
             <div>
                 <h3 class="level txt_body">{{ gig.owner.level }}</h3>
             </div>
         </section>
-            <h3 class="title_product txt_body">{{ gig?.title }}</h3>
-            <div class ="rate">
-             <span v-html="$svg('blackStar')"></span> 
+        <h3 class="title_product txt_body">{{ gig?.title }}</h3>
+        <div class="rate">
+            <span v-html="$svg('blackStar')"></span>
             <p class="txt_body">{{ gig?.owner.rate }}</p>
-            </div>            
-          <p class="price_product txt_body">
-               From ${{ gig.price?.toLocaleString() }}
-            </p>
-            <!-- <button @click="removeGig(gig._id)">x</button> -->
-            <hr />
-        </li>
-        <!-- </ul> -->
-        <!-- <form @submit.prevent="addGig()">
+        </div>
+        <p class="price_product txt_body">
+            From ${{ gig.price?.toLocaleString() }}
+        </p>
+        <!-- <button @click="removeGig(gig._id)">x</button> -->
+        <hr />
+    </li>
+    <!-- </ul> -->
+    <!-- <form @submit.prevent="addGig()">
             <h2>Add gig</h2>
             <input type="text" v-model="gigToAdd.title" />
             <button>Save</button>
@@ -34,7 +46,8 @@
 </template>
 
 <script>
-
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
 
 export default {
     name: 'GigPreview',
@@ -43,7 +56,7 @@ export default {
     },
     data() {
         return {
-
+            slides: []
         }
     },
     computed: {
@@ -51,13 +64,27 @@ export default {
             return this.$store.getters.gigs
         }
     },
+    watch: {
+        gig: {
+            handler(newGig) {
+                this.slides = newGig.imgUrls.map(imageUrl => ({
+                    image: imageUrl
+                }))
 
+            },
+            immediate: true // Add this if you want to call the handler immediately when the component is created
+        },
+    },
     methods: {
         goToDetail() {
             this.$router.push(`/gig/${this.gig._id}`)
         },
-
+        preventClick(event) {
+            event.preventDefault();
+            event.stopPropagation();
+        },
     },
+    components: { VueperSlides, VueperSlide, },
 }
 
 </script>
