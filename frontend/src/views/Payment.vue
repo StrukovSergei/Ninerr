bc6865fdda74b8ebdf3bd30c27e931bf79505a40
StrukovSergei <strukov.sergei.999@gmail.com>
2023-07-29 10:21:02 +0300

    finished payment page + path

--- frontend/src/views/Payment.vue
+++ frontend/src/views/Payment.vue
@@ -49,14 +49,33 @@
                             </div>
                         </template>
                     </div>
+                    <div class="payment-fees flex">
+                        <p class="payment-fee">
+                            <span class="payment-fee-label">Service Fee</span>
+                            <span class="payment-fee-value">US${{ serviceFee.toFixed(2) }}</span>
+                        </p>
+                        <p class="payment-vat">
+                            <span class="payment-vat-label">VAT</span>
+                            <span class="payment-vat-value">US${{ vat.toFixed(2) }}</span>
+                        </p>
+                        <p class="payment-total fs20">
+                            <span class="payment-total-label">Total</span>
+                            <span class="payment-total-value">US${{ total.toFixed(2) }}</span>
+                        </p>
+                        <p class="payment-delivery">
+                            <span class="payment-delivery-label">Total delivery time</span>
+                            <span class="payment-total-value">{{ gig.daysToMake }}</span>
+                        </p>
+                    </div>
                 </div>
 
-                <button class="btn-continue">
-
-                    <span class="btn-continue-arrow" aria-hidden="true" style="width: 16px; height: 16px; fill: white;">
-                        <span v-html="$svg('arrowRight')"></span>
-                    </span>
-                </button>
+                <!-- <RouterLink class="btn-confirm-route"
+                    :to="{ name: 'UserDetails', params: { gigId: gig.owner?._id }, props: { gig: gig } }"> -->
+                <RouterLink v-if="user" :to="{ name: 'UserDetails', params: { id: user._id } }">
+                    <button class="btn-confirm">Confirm & Pay</button>
+                </RouterLink>
+                <button v-else class="btn-confirm" @click="handleConfirmAndPay()">Login to Confirm & Pay</button>
+                <!-- </RouterLink> -->
 
 
             </section>
@@ -74,6 +93,7 @@ export default {
     data() {
         return {
             gig: null,
+            user: null,
         }
     },
     async created() {
@@ -81,6 +101,7 @@ export default {
             const user = userService.getLoggedinUser()
             if (user) {
                 this.$store.commit({ type: "setLoggedinUser", user })
+                this.user = user
             }
 
             const { gigId } = this.$route.params
@@ -92,7 +113,27 @@ export default {
             console.error(err)
         }
     },
+    computed: {
+        serviceFee() {
+            return this.gig ? this.gig.price * 0.15 : 0
+        },
 
+        vat() {
+            return this.gig ? this.gig.price * 0.17 : 0
+        },
+
+        total() {
+            return this.gig ? this.gig.price + this.serviceFee + this.vat : 0
+        },
+    },
+    methods: {
+        handleConfirmAndPay() {
+
+            alert('Login required')
+
+        }
+
+    }
 }
 
 </script>
