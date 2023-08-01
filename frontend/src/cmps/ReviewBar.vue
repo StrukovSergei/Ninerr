d6f02cd7e25593e015dd5d7494aa64ae9f858b8e
StrukovSergei <strukov.sergei.999@gmail.com>
2023-08-01 11:24:27 +0300

    added stars to reviewbar

--- frontend/src/cmps/ReviewBar.vue
+++ frontend/src/cmps/ReviewBar.vue
@@ -1,11 +1,37 @@
 <template>
-    <table class="rate-bars">
-        <tr v-for="i in stars" class="row" :class="{'faded' : !getRateCount(i)}">
-            <span class="num">{{ i }}</span> <span>Stars</span> <td class="progress-bar-container">
-            <td class="progress-bar" :style="{ width: `${getPrecentage(i)}%` }"></td>
-            </td><span>({{ getRateCount(i) }})</span>
-        </tr>
-    </table>
+    <div>
+        <div class="rate-summary ">
+            <span class="rate-review-number">{{ reviews.length }} reviews for this Gig
+
+                <div class="rate-avg">
+                    <span class="rate-avg-stars">
+                        <span v-for="(star, index) in generateStars(averageRating).fullStars" :key="index">
+                            <span v-html="$svg('star')"></span>
+                        </span>
+                        <span v-for="(star, index) in generateStars(averageRating).emptyStars" :key="index">
+                            <span v-html="$svg('emptyStar')"></span>
+                        </span>
+                        <span class="rate-avg-number">{{ averageRating.toFixed(1) }}</span>
+                    </span>
+                </div>
+
+            </span>
+        </div>
+
+        <table class="rate-bars">
+            <tr v-for="i in stars" class="row" :class="{ 'faded': !getRateCount(i) }">
+                <span class="num">{{ i }}</span>
+                <span>Stars</span>
+                <td class="progress-bar-container">
+                <td class="progress-bar" :style="{ width: `${getPercentage(i)}%` }"></td>
+                </td>
+                <span>({{ getRateCount(i) }})</span>
+            </tr>
+        </table>
+
+        <!-- Your existing review list template goes here -->
+        <!-- ... -->
+    </div>
 </template>
 
 <script>
@@ -20,14 +46,34 @@ export default {
     },
     methods: {
         getRateCount(num) {
-            const countRate = this.reviews.reduce((count, review) => (review.rate === num ? count + 1 : count), 0)
+            const countRate = this.reviews.reduce((count, review) => {
+                const reviewRate = review.rate >= 4.5 ? Math.ceil(review.rate) : Math.floor(review.rate)
+                return reviewRate === num ? count + 1 : count
+            }, 0)
             return countRate
         },
-        getPrecentage(num) {
-            const sum = this.getRateCount(1) + this.getRateCount(2) + this.getRateCount(3) + this.getRateCount(4) + this.getRateCount(5)
-
-            return this.getRateCount(num) / sum * 100
+        getPercentage(num) {
+            const sum = this.stars.reduce((total, star) => total + this.getRateCount(star), 0)
+            const rateCount = this.getRateCount(num)
+            return (rateCount / sum) * 100
+        },
+        generateStars(rate) {
+            const fullStars = Math.floor(rate)
+            const emptyStars = 5 - fullStars
+            return {
+                fullStars: Array(fullStars).fill(1),
+                emptyStars: Array(emptyStars).fill(0),
+            }
+        },
+    },
+    computed: {
+        averageRating() {
+            if (this.reviews.length === 0) {
+                return 0
+            }
+            const totalRating = this.reviews.reduce((sum, review) => sum + review.rate, 0)
+            return totalRating / this.reviews.length
         }
-    }
+    },
 }
 </script>
