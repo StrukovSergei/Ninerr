2610f4bfa8021c5f1a60fe2ff79048248f384535
StrukovSergei <strukov.sergei.999@gmail.com>
2023-09-10 10:18:25 +0300

    added user details in seller page

--- frontend/src/views/SellerPage.vue
+++ frontend/src/views/SellerPage.vue
@@ -1,114 +1,142 @@
 <template>
-    <div class="seller-box">
-        <section v-if="user" class="main-seller flex">
-            <!-- <span class="top-btns flex">
+    <div class="grid seller-page-helper">
+        <div v-if="user" class="user-container flex">
+            <h1 class="user-name">{{ user.fullname }}</h1>
+            <div class="user-photo">
+                <img v-if="user.imgUrl" :src="user.imgUrl" alt="User Photo" style="max-width: 200px" />
+                <div v-else class="user-initials">
+                    {{ user.fullname.charAt(0).toUpperCase() }}
+                </div>
+            </div>
+            <div class="total-orders">Total orders</div>
+            <div class="total-orders-num">{{ orders.length }}</div>
+            <div class="user-country-title">From</div>
+            <div class="user-country">
+                Israel
+                <img class="user-coutry-img"
+                    src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1ee-1f1f1.png" alt="" />
+            </div>
+            <div class="member">Member since </div>
+            <div class="year">2023</div>
+            <div class="member">Last active </div>
+            <div class="active">online</div>
+            <!-- <RouterLink class="seller-btn" :to="becomeSeller">Become a Seller
+      </RouterLink> -->
+            <!-- <a class="seller-btn" @click="onLogout()">Logout</a> -->
+            <!-- <img style="max-width: 200px;" :src="user.imgUrl" /> -->
+        </div>
+        <div class="seller-box">
+            <section v-if="user" class="main-seller flex">
+                <!-- <span class="top-btns flex">
                 <button @click="onLogout()"><span>Logout</span></button>
 
                 <RouterLink :to="userProfile">Gigs I bought</RouterLink>
             </span> -->
-            <add-gig-modal :is-modal-open="isModalOpen" @close="closeModal" @add="handleAddGig"></add-gig-modal>
-            <section class="anlaytics-for-mobile">
-                <el-row class="earning-table">
-                    <el-col :span="7">
-                        <section>
-                            <h4>Completed</h4>
-                            <span class="earnings-num">{{ getTotalEarningsWithoutPendingOrders() }}</span>
-                        </section>
-                    </el-col>
+                <!-- <add-gig-modal :is-modal-open="isModalOpen" @close="closeModal" @add="handleAddGig"></add-gig-modal> -->
 
-                    <el-col :span="8">
-                        <section>
-                            <h4 class="accepted-title">Accepted</h4>
-                            <span class="total-earning">{{ numberOfInProgressOrders }}</span>
-                        </section>
-                    </el-col>
+                <section class="anlaytics-for-mobile">
+                    <el-row class="earning-table">
+                        <el-col :span="7">
+                            <section>
+                                <h4>Completed</h4>
+                                <span class="earnings-num">{{ getTotalEarningsWithoutPendingOrders() }}</span>
+                            </section>
+                        </el-col>
 
-                    <el-col :span="8">
-                        <section>
-                            <h4>Pending</h4>
-                            <span class="pending-num">{{ getNumberOfPendingOrders() }}</span>
-                        </section>
-                    </el-col>
-                </el-row>
-                <div class="analytics-rows">
-                    <div class="flex">
-                        <h2>Earing in August</h2>
-                        <h1>${{ totalEarningsFromAllOrders }}</h1>
+                        <el-col :span="8">
+                            <section>
+                                <h4 class="accepted-title">Accepted</h4>
+                                <span class="total-earning">{{ numberOfInProgressOrders }}</span>
+                            </section>
+                        </el-col>
+
+                        <el-col :span="8">
+                            <section>
+                                <h4>Pending</h4>
+                                <span class="pending-num">{{ getNumberOfPendingOrders() }}</span>
+                            </section>
+                        </el-col>
+                    </el-row>
+                    <div class="analytics-rows">
+                        <div class="flex">
+                            <h2>Earing in August</h2>
+                            <h1>${{ totalEarningsFromAllOrders }}</h1>
+                        </div>
+                        <div class="flex">
+                            <h2>Avg. selling price</h2>
+                            <h1>$229.23</h1>
+                        </div>
+                        <div class="flex">
+                            <h2>Active orders</h2>
+                            <h1>
+                                {{ numberOfInProgressOrders }}<span> (${{ earningsInProgressOrders }})</span>
+                            </h1>
+                        </div>
                     </div>
-                    <div class="flex">
-                        <h2>Avg. selling price</h2>
-                        <h1>$229.23</h1>
+                </section>
+                <section class="manage-gigs-container">
+                    <p class="Manage-gigs">Manage gigs</p>
+                    <!-- <button class="addgig-btn" @click="openModal">Add Gig</button> -->
+                    <div v-if="gigs && gigs.length" class="Manage-gigsT">
+                        <el-table class="gigsT" :border="true" :data="gigs" style="width: 100%">
+                            <el-table-column prop="title" label="Gig" width="450" class-name="custom-title-column" />
+                            <el-table-column prop="price" label="Price" width="120" class-name="custom-price-column" />
+                            <el-table-column prop="status" label="Status" width="160" class-name="custom-column">
+                                <template #default="{ row }">
+                                    <el-button :class="getStatusButtonClass(row.status)">{{
+                                        row.status
+                                    }}</el-button>
+                                </template>
+                            </el-table-column>
+                            <el-table-column prop="status" label="Update Status" width="160">
+                                <template #default="{ row }">
+                                    <select v-model="row.status" @change="updateGigStatus(row)">
+                                        <option v-for="status in gigStatusOptions" :key="status">
+                                            {{ status }}
+                                        </option>
+                                    </select>
+                                </template>
+                            </el-table-column>
+                            <!-- Add the new column for the remove button -->
+                            <el-table-column label="Remove" width="100">
+                                <template #default="{ row }">
+                                    <el-button @click="removeGig(row._id)" type="danger" icon="el-icon-delete">X</el-button>
+                                </template>
+                            </el-table-column>
+                        </el-table>
                     </div>
-                    <div class="flex">
-                        <h2>Active orders</h2>
-                        <h1>
-                            {{ numberOfInProgressOrders }}<span> (${{ earningsInProgressOrders }})</span>
-                        </h1>
+                    <div v-else class="No-gigs-available">
+                        <p>No gigs available.</p>
                     </div>
-                </div>
+                </section>
             </section>
-            <section class="manage-gigs-container">
-                <p class="Manage-gigs">Manage gigs</p>
-                <button class="addgig-btn" @click="openModal">Add Gig</button>
-                <div v-if="gigs && gigs.length" class="Manage-gigsT">
-                    <el-table class="gigsT" :border="true" :data="gigs" style="width: 100%">
-                        <el-table-column prop="title" label="Gig" width="450" class-name="custom-title-column" />
-                        <el-table-column prop="price" label="Price" width="120" class-name="custom-price-column" />
-                        <el-table-column prop="status" label="Status" width="160" class-name="custom-column">
-                            <template #default="{ row }">
-                                <el-button :class="getStatusButtonClass(row.status)">{{
-                                    row.status
-                                }}</el-button>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="status" label="Update Status" width="160">
-                            <template #default="{ row }">
-                                <select v-model="row.status" @change="updateGigStatus(row)">
-                                    <option v-for="status in gigStatusOptions" :key="status">
-                                        {{ status }}
-                                    </option>
-                                </select>
-                            </template>
-                        </el-table-column>
-                        <!-- Add the new column for the remove button -->
-                        <el-table-column label="Remove" width="100">
-                            <template #default="{ row }">
-                                <el-button @click="removeGig(row._id)" type="danger" icon="el-icon-delete">X</el-button>
-                            </template>
-                        </el-table-column>
-                    </el-table>
-                </div>
-                <div v-else class="No-gigs-available">
-                    <p>No gigs available.</p>
-                </div>
-            </section>
-        </section>
-        <section class="manage-orders-container">
-            <p class="Manage-orders">Manage orders</p>
-            <div class="demo-collapse" :class="{ 'dark-overlay': ifStatusOpen }">
-                <el-collapse accordion>
-                    <el-collapse-item class="order-title" v-for="order in orders" :key="order.id" :name="order.id">
-                        <!-- :title="`${order.buyerName} - <span>${order.status}</span>`"  -->
-                        <template #title>
-                            <div class="collapse-title flex">
-                                <span> {{ order.buyerName }} </span> <span :class="getStatusClass(order.status)">{{
-                                    order.status
-                                }}</span>
-                            </div>
-                            <el-icon class="header-icon">
+            <section class="manage-orders-container">
+                <p class="Manage-orders">Manage orders</p>
+                <div class="demo-collapse" :class="{ 'dark-overlay': ifStatusOpen }">
+                    <el-collapse accordion>
+                        <el-collapse-item class="order-title" v-for="order in orders" :key="order.id" :name="order.id">
+                            <!-- :title="`${order.buyerName} - <span>${order.status}</span>`"  -->
+                            <template #title>
+                                <div class="collapse-title flex">
+                                    <span> {{ order.buyerName }} </span> <span :class="getStatusClass(order.status)">{{
+                                        order.status
+                                    }}</span>
+                                </div>
+                                <el-icon class="header-icon">
 
-                            </el-icon>
-                        </template>
-                        <div>
-                            <p><span class="order-inner-img"><img :src="order.imgUrl" alt=""></span> <span
-                                    class="order-inner-span">{{ order.gigTitle }}</span></p>
-                            <p>Status <span class="order-inner-span">{{ order.status }}</span></p>
-                            <p>Price<span class="order-inner-span">${{ order.price }}</span></p>
-                            <p>Buyer <span class="order-inner-span">{{ order.buyerName }}</span></p>
-                            <a class="status-btn" @click="toggleStatusModal">Change status <span v-html="$svg('arrowDown')"></span></a>
-                            <StatusModal v-if="ifStatusOpen" :order="order" @status-changed="updateStatus">
-                            </StatusModal>
-                            <!-- <el-dropdown placement="top-end">
+                                </el-icon>
+                            </template>
+                            <div>
+                                <p><span class="order-inner-img"><img :src="order.imgUrl" alt=""></span> <span
+                                        class="order-inner-span">{{ order.gigTitle }}</span></p>
+                                <p>Status <span class="order-inner-span">{{ order.status }}</span></p>
+                                <p>Price<span class="order-inner-span">${{ order.price }}</span></p>
+                                <p>Buyer <span class="order-inner-span">{{ order.buyerName }}</span></p>
+                                <a class="status-btn" @click="toggleStatusModal">Change status <span
+                                        v-html="$svg('arrowDown')"></span></a>
+                                <StatusModal v-if="ifStatusOpen" :order="order" @status-changed="updateStatus">
+                                </StatusModal>
+                                <!-- <el-dropdown placement="top-end">
                             <span class="el-dropdown-link">
                                 Change status
                                 <span v-html="$svg('arrowDown')"></span>
@@ -122,11 +150,12 @@
                                 </el-dropdown-menu>
                             </template>
                         </el-dropdown> -->
-                        </div>
-                    </el-collapse-item>
-                </el-collapse>
-            </div>
-        </section>
+                            </div>
+                        </el-collapse-item>
+                    </el-collapse>
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 
@@ -187,8 +216,8 @@ export default {
             return this.orders
                 .reduce((total, order) => {
                     if (order.status !== "rejected" && order.status !== "pending") {
-                        const orderPrice = parseFloat(order.price) 
-                        return total + orderPrice 
+                        const orderPrice = parseFloat(order.price)
+                        return total + orderPrice
                     }
                     return total;
                 }, 0)
